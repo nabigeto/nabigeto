2cdc98c77edda4bd4baa30a0517a863f48b50f9e
Gavin <nabigeto@gmail.com>
2020-01-18 23:21:55 +0000

    1.1

--- app/src/main/java/com/nabigeto/gavin/nabigetonabi/FrontScreenLoader/FrontScreenLoader.java
+++ app/src/main/java/com/nabigeto/gavin/nabigetonabi/FrontScreenLoader/FrontScreenLoader.java
@@ -2,7 +2,11 @@ package com.nabigeto.gavin.nabigetonabi.FrontScreenLoader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.nabigeto.gavin.nabigetonabi.MainActivity;
+import com.nabigeto.gavin.nabigetonabi.R;
 
 public class FrontScreenLoader extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class FrontScreenLoader extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_screen_loader);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
+
+
---
+++ app/src/main/java/com/nabigeto/gavin/nabigetonabi/Lists/my_lists.java
@@ -0,0 +1,6 @@
+package com.nabigeto.gavin.nabigetonabi.Lists;
+
+import androidx.fragment.app.Fragment;
+
+public class my_lists extends Fragment {
+}
